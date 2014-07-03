449e4764d6059d6bbc4c5c502ca6d3837b8378ba
vandosant <sjskender@gmail.com>
2014-07-03 08:14:05 -0600

    Users can advance the game

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'lib/game_of_life'
 require_relative 'lib/grid_generator'
 
 class App < Sinatra::Base
-  set :game, GameOfLife.new(0,0)
+  set :game, GameOfLife.new(0, 0)
   set :grid, GridGenerator.map(settings.game.cells)
 
   get '/' do
@@ -11,9 +11,15 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    settings.game = GameOfLife.new(params['grid-size'].to_i, params['live-cells'].to_i)
-    settings.game.generate_cells
-    settings.grid = GridGenerator.map(settings.game.cells)
-    redirect '/'
+    if params['advance']
+      settings.game.advance
+      settings.grid = GridGenerator.map(settings.game.cells)
+      redirect '/'
+    else
+      settings.game = GameOfLife.new(params['grid-size'].to_i, params['live-cells'].to_i)
+      settings.game.generate_cells
+      settings.grid = GridGenerator.map(settings.game.cells)
+      redirect '/'
+    end
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -13,4 +13,19 @@ feature 'playing the game of life' do
     expect(page).to have_content "0"
     expect(page).to have_content "1"
   end
+
+  scenario 'advance a game' do
+    visit '/'
+
+    expect(page).to have_content "Game of Life"
+
+    fill_in "Grid Size", with: 4
+    fill_in "Live Cells", with: 1
+    click_button "New Game"
+
+    click_button "Advance"
+
+    expect(page).to have_content "0"
+    expect(page).to_not have_content "1"
+  end
 end
