37203b54335a95d560fed4f1f91cad87988c8b3c
vandosant <sjskender@gmail.com>
2014-07-09 17:14:13 -0600

    Removed unnecessary code
    * Case where 2 live cells always die is now handled by the entire method

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -22,74 +22,69 @@ class GameOfLife
   end
 
   def advance
-    if @cells.count(1) <= 2
-      @cells.each_with_index do |cell, index|
-        @cells[index] = 0 if cell == 1
-      end
-    else
-      grid = get_grid
-      x_max = grid.map { |coordinate| coordinate[0] }.sort.last
-      y_max = grid.map { |coordinate| coordinate[1] }.sort.last
-      dead_cell_indexes = []
-      new_live_cell_indexes = []
+    grid = get_grid
+    x_max = grid.map { |coordinate| coordinate[0] }.sort.last
+    y_max = grid.map { |coordinate| coordinate[1] }.sort.last
+    dead_cell_indexes = []
+    new_live_cell_indexes = []
 
-      grid.each_with_index do |cell_coordinate, index|
-        live_neighbor_count = 0
-        x_value = cell_coordinate[0]
-        y_value = cell_coordinate[1]
+    grid.each_with_index do |cell_coordinate, index|
+      live_neighbor_count = 0
+      x_value = cell_coordinate[0]
+      y_value = cell_coordinate[1]
 
-        if left_neighbor_present?(x_value) && @cells[index-1] == 1
-          live_neighbor_count += 1
-        end
-
-        if right_neighbor_present?(x_value, x_max) && @cells[index+1] == 1
-          live_neighbor_count += 1
-        end
-
-        if bottom_neighbor_present?(y_value, y_max) && @cells[grid.index([x_value, (y_value + 1)])] == 1
-          live_neighbor_count += 1
-        end
-
-        if top_neighbor_present?(y_value) && @cells[grid.index([x_value, (y_value - 1)])] == 1
-          live_neighbor_count += 1
-        end
-
-        if live_neighbor_count < 2 || live_neighbor_count > 3
-          dead_cell_indexes << index
-        elsif live_neighbor_count == 3
-          new_live_cell_indexes << index
-        end
+      if left_neighbor_present?(x_value) && @cells[index-1] == 1
+        live_neighbor_count += 1
       end
 
-      dead_cell_indexes.each do |index|
-        @cells[index] = 0
+      if right_neighbor_present?(x_value, x_max) && @cells[index+1] == 1
+        live_neighbor_count += 1
       end
 
-      new_live_cell_indexes.each do |index|
-        @cells[index] = 1
+      if bottom_neighbor_present?(y_value, y_max) && @cells[grid.index([x_value, (y_value + 1)])] == 1
+        live_neighbor_count += 1
+      end
+
+      if top_neighbor_present?(y_value) && @cells[grid.index([x_value, (y_value - 1)])] == 1
+        live_neighbor_count += 1
+      end
+
+      if live_neighbor_count < 2 || live_neighbor_count > 3
+        dead_cell_indexes << index
+      elsif live_neighbor_count == 3
+        new_live_cell_indexes << index
       end
     end
-  end
 
-  private
+    dead_cell_indexes.each do |index|
+      @cells[index] = 0
+    end
 
-  def get_grid
-    GridGenerator.map(@cells)
-  end
+    new_live_cell_indexes.each do |index|
+      @cells[index] = 1
+    end
+end
 
-  def left_neighbor_present?(x_coordinate)
-    x_coordinate != 0
-  end
+private
 
-  def right_neighbor_present?(x_coordinate, x_max)
-    x_coordinate != x_max
-  end
+def get_grid
+  GridGenerator.map(@cells)
+end
 
-  def top_neighbor_present?(y_coordinate)
-    y_coordinate != 0
-  end
+def left_neighbor_present?(x_coordinate)
+  x_coordinate != 0
+end
 
-  def bottom_neighbor_present?(y_coordinate, y_max)
-    y_coordinate != y_max
-  end
+def right_neighbor_present?(x_coordinate, x_max)
+  x_coordinate != x_max
+end
+
+def top_neighbor_present?(y_coordinate)
+  y_coordinate != 0
+end
+
+def bottom_neighbor_present?(y_coordinate, y_max)
+  y_coordinate != y_max
+end
+
 end
