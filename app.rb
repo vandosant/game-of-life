73196e5be08f4e21fd01d3d6333245a7b5677882
vandosant <sjskender@gmail.com>
2014-07-10 14:44:17 -0600

    Refactoring error handling
    * Removed complexity of extra variable to track status

--- app.rb
+++ app.rb
@@ -5,24 +5,18 @@ require_relative 'lib/grid_generator'
 class App < Sinatra::Base
   set :game, GameOfLife.new(0, 0)
   set :grid, GridGenerator.map(settings.game.cells)
-  set :error_message, nil
   set :grid_size_error_message, 'Grid size must be divisible by ten'
   set :live_cell_error_message, 'Live cells must be less than grid size'
 
   get '/' do
-    if errors?
-      settings.error_message = nil
-    end
     erb :index, locals: {:game => settings.game, :grid => settings.grid}
   end
 
   post '/' do
     if grid_size_error?(params)
-      settings.error_message = settings.grid_size_error_message
-      erb :index, locals: {:game => settings.game, :grid => settings.grid, :error_message => settings.error_message}
+      erb :index, locals: {:game => settings.game, :grid => settings.grid, :error_message => settings.grid_size_error_message}
     elsif live_cells_error?(params)
-      settings.error_message = settings.live_cell_error_message
-      erb :index, locals: {:game => settings.game, :grid => settings.grid, :error_message => settings.error_message}
+      erb :index, locals: {:game => settings.game, :grid => settings.grid, :error_message => settings.live_cell_error_message}
     elsif params['advance']
       settings.game.advance
       settings.grid = GridGenerator.map(settings.game.cells)
@@ -36,10 +30,6 @@ class App < Sinatra::Base
   end
 
   private
-
-  def errors?
-    settings.error_message != nil
-  end
 
   def grid_size_error?(params)
     params['grid-size'].to_i % 10 != 0
